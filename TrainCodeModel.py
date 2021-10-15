c066ae760809a54e64c2c974d1e2d55159b28057
LSH9832 <69880398+LSH9832@users.noreply.github.com>
2021-10-15 17:03:22 +0800

    Delete TrainCodeModel.py

--- TrainCodeModel.py
+++
@@ -1,9 +0,0 @@
-import sys
-import pickle
-module_path = "$MODULE_PATH"
-sys.path.append(module_path)
-
-if __name__ == '__main__':
-    from packages.yolov5 import data_train
-    option = pickle.load(open("option", 'rb'))
-    data_train.start_train(option)
