a97611783ec4ebabfbb7ecf72ddeaadfc66ad71e
LSH9832 <69880398+LSH9832@users.noreply.github.com>
2021-10-15 19:25:46 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 pip3 install -r ./res/requirements.txt
 python3 ./res/CreateRunFile.py
-cd ./packages/yolov5/models/pt
+mkdir ./packages/yolov5/models/pt
+# cd ./packages/yolov5/models/pt
 # wget https://github.com/ultralytics/yolov5/releases/download/v5.0/yolov5s.pt
 # wget https://github.com/ultralytics/yolov5/releases/download/v5.0/yolov5m.pt
 # wget https://github.com/ultralytics/yolov5/releases/download/v5.0/yolov5l.pt
